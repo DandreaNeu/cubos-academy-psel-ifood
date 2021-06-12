2ac834ac431832fab73228adf69051ba06e1b666
Dandrea Neumann <dandreaneumann2@hotymail.com>
2021-06-12 00:34:48 -0300

    Exercicio desafiador

---
+++ MeSegueNoInsta.js
@@ -0,0 +1,10 @@
+function solucao(largura, altura) {
+  
+    if(altura<largura)
+       console.log("PAISAGEM")
+    if(altura==largura)
+        console.log("QUADRADA")
+    if(altura>largura)
+        console.log("RETRATO")
+        
+}
